2d8faf7ecacccf72582ca705e41b46ddc320556c
gary-stroup-developer <stroup.uxuidesign@gmail.com>
2023-06-18 11:01:39 -0700

    worked on welcome component, added in custom fonts, worked on screenHeaderBtn component, and Home screen

--- app/_layout.js
+++ app/_layout.js
@@ -1,7 +1,26 @@
 import { Stack } from "expo-router";
+import { useCallback } from "react";
+import { useFonts } from "expo-font"; //added this to handle custom fonts error
+import * as SplashScreen from 'expo-splash-screen';
 
- const Layout = () => {
-  return <Stack />;
+//when app is initially loading, this will make splashcreen visible
+SplashScreen.preventAutoHideAsync();
+
+const Layout = () => {
+  const [fontsLoaded] = useFonts({
+    DMBold: require('../assets/fonts/DMSans-Bold.ttf'),
+    DMMedium: require('../assets/fonts/DMSans-Medium.ttf'),
+    DMRegular: require('../assets/fonts/DMSans-Regular.ttf'),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      //hide the splashscreen immediately since fonts have been loaded
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded])
+  if (!fontsLoaded) return null;
+  return <Stack onLayout={onLayoutRootView } />;
 }
 
 export default Layout
