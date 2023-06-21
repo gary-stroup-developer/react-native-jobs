be3b88ac16ad6f86f366d94523c3d8a0ff9a6c94
gary-stroup-developer <stroup.uxuidesign@gmail.com>
2023-06-21 16:58:42 -0700

    completed jobDetails tab components that hold info about job. About,Qualifications,Responsibilities

--- components/jobdetails/company/Company.jsx
+++ components/jobdetails/company/Company.jsx
@@ -1,12 +1,37 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React from 'react';
+import { View, Text, Image } from 'react-native';
 
 import styles from './company.style'
+import { icons } from '../../../constants';
+import { checkImageURL } from '../../../utils';
 
-const Company = () => {
+const Company = ({ companyLogo, jobTitle, companyName, location }) => {
+  
   return (
-    <View>
-      <Text>Company</Text>
+    <View style={styles.container}>
+      <View style={styles.logoBox}>
+        <Image
+          source={{
+            uri: checkImageURL(companyLogo) ? companyLogo
+              : 'https://w7.pngwing.com/pngs/271/479/png-transparent-career-development-computer-icons-job-employment-career-coach-blue-logo-employment.png'
+          }}
+          style={styles.logoImage}
+        />
+      </View>
+      <View style={styles.jobTitleBox}>
+        <Text style={styles.jobTitle}>{ jobTitle}</Text> 
+      </View>
+      <View style={styles.companyInfoBox}>
+        <Text style={styles.companyName}>{companyName}</Text>
+        <View style={styles.locationBox}>
+          <Image
+            source={icons.location}
+            resizeMode='contain'
+            style={styles.locationImage}
+          />
+          <Text style={styles.locationName}>{location}</Text>
+        </View>
+      </View>
     </View>
   )
 }
