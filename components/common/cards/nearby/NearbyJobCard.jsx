4cf8f83aac71b1ee9ae47ace38ffe9d466b0c61f
gary-stroup-developer <stroup.uxuidesign@gmail.com>
2023-06-21 15:43:23 -0700

    completed NearbyJobCard component. set up [id].js file. Start building Company component

--- components/common/cards/nearby/NearbyJobCard.jsx
+++ components/common/cards/nearby/NearbyJobCard.jsx
@@ -7,17 +7,17 @@ import { checkImageURL } from '../../../../utils';
 const NearbyJobCard = ({ job, handleNavigate }) => {
   return (
     <TouchableOpacity style={styles.container} onPress={handleNavigate}>
-      <TouchableOpacity style={styles.logoContainer(selectedJob, item)}>
+      <TouchableOpacity style={styles.logoContainer}>
         <Image
-          source={{ uri: checkImageURL(item.employer_logo) ? item.employer_logo : 'https://w7.pngwing.com/pngs/271/479/png-transparent-career-development-computer-icons-job-employment-career-coach-blue-logo-employment.png' }}
+          source={{ uri: checkImageURL(job.employer_logo) ? job.employer_logo : 'https://w7.pngwing.com/pngs/271/479/png-transparent-career-development-computer-icons-job-employment-career-coach-blue-logo-employment.png' }}
           resizeMode="contain"
-          style={styles.logoImage}
+          style={styles.logImage}
         />
       </TouchableOpacity>
-      <Text style={styles.companyName} numberOfLines={1}>{item.employer_name}</Text>
-      <View style={styles.infoContainer}>
-        <Text style={styles.jobName(selectedJob, item)} numberOfLines={1}>{item.job_title}</Text>
-        <Text style={styles.location}>{item.job_country}</Text>
+      
+      <View style={styles.textContainer}>
+        <Text style={styles.jobName} numberOfLines={1}>{job.job_title}</Text>
+        <Text style={styles.jobType}>{job.job_employment_type}</Text>
       </View>
     </TouchableOpacity>
   )
